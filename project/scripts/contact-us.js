74dc38abe28ef9a98422dfaefeb552090ec5534e
CFrancoChavez <franco.chvez@gmail.com>
2024-06-06 22:34:43 -0300

    contact us pages changes

--- project/scripts/contact-us.js
+++ project/scripts/contact-us.js
@@ -1,3 +1,12 @@
+const mainnav = document.querySelector(".navigation");
+const hambutton = document.querySelector("#menu");
+
+// Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
+hambutton.addEventListener("click", () => {
+  mainnav.classList.toggle("show");
+  hambutton.classList.toggle("show");
+});
+
 document.addEventListener('DOMContentLoaded', function() {
     const form = document.getElementById('contactForm');
     const successMessage = document.getElementById('successMessage');
